d57e88a9216112a16172cc0932c80fc670f14ee0
Gotla06 <cse24-026@thuto.bac.ac.bw>
2025-05-01 11:49:43 +0200

    commit changes

---
+++ script.js
@@ -0,0 +1,6 @@
+// DOM Ready
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Bootstrap tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+       
